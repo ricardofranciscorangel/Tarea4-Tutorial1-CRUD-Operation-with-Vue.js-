852070f5b270beb27f54c457caacb464ea91ae4f
Reach <L21390371@chetumal.tecnm.mx>
2026-05-29 12:49:06 -0500

    paso 4 conexicon con axios

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,19 +1,43 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const posts = ref([]);
+
+const fetchPosts = async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/posts');
+    posts.value = response.data;
+  } catch (error) {
+    console.error('Error al traer los posts:', error);
+  }
+};
+
+onMounted(() => {
+  fetchPosts();
+});
+</script>
+
 <template>
-  <h1>All Post</h1>
-  <table id="posts">
-    <tr>
-      <th>S.N</th>
-      <th>Title</th>
-      <th>Action</th>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>Hello World!</td>
-      <td>
-        <button class="btn btn-green">O</button>
-        <button class="btn btn-blue">/</button>
-        <button class="btn btn-red">X</button>
-      </td>
-    </tr>
+  <h1>Todos los Posts</h1>
+ <table id="posts">
+    <thead>
+      <tr>
+        <th>N.º</th>
+        <th>Título</th>
+        <th>Acción</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(post, index) in posts" :key="post.id">
+        <td>{{ index + 1 }}</td>
+        <td>{{ post.title }}</td>
+        <td>
+          <button class="btn btn-green">Ver</button>
+          <button class="btn btn-blue">Editar</button>
+          <button class="btn btn-red">Borrar</button>
+        </td>
+      </tr>
+    </tbody>
   </table>
 </template>
